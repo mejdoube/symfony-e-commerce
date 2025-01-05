ff2f6931ae18775623520242146395744e6cc291
nourhen595 <nourhen595@gmail.com>
2025-01-05 17:14:39 +0100

    add db

---
+++ categories.sql
@@ -0,0 +1,8 @@
+INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (33, null, 'Informatique', 'informatique', 0);
+INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (34, 33, 'Ordinateurs portables', 'ordinateurs-portables', 0);
+INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (35, 33, 'Ecrans', 'ecrans', 0);
+INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (36, 33, 'Souris', 'souris', 0);
+INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (37, null, 'Mode', 'mode', 0);
+INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (38, 37, 'Homme', 'homme', 0);
+INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (39, 37, 'Femme', 'femme', 0);
+INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (40, 37, 'Enfant', 'enfant', 0);
