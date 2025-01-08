fcfb5604ed3abf5082769f2b9aa46345767bed92
fathi <fathi3ayari333@gmail.com>
2025-01-08 17:49:46 +0100

    add db and the migration file

--- categories.sql
+++ categories.sql
@@ -1,7 +1,8 @@
-INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (34, 33, 'Ordinateurs portables', 'ordinateurs-portables', 0);
-INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (35, 33, 'Ecrans', 'ecrans', 0);
-INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (36, 33, 'Souris', 'souris', 0);
-INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (37, null, 'Mode', 'mode', 0);
-INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (38, 37, 'Homme', 'homme', 0);
-INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (39, 37, 'Femme', 'femme', 0);
-INSERT INTO ecommerce_n.categories (id, parent_id, name, slug, category_order) VALUES (40, 37, 'Enfant', 'enfant', 0);
+INSERT INTO `e-commerce`.categories (id, parent_id, name, category_order, slug) VALUES (33, null, 'IT', 0, 'IT');
+INSERT INTO `e-commerce`.categories (id, parent_id, name, category_order, slug) VALUES (34, 33, 'Ordinateurs portables', 0, 'ordinateurs-portables');
+INSERT INTO `e-commerce`.categories (id, parent_id, name, category_order, slug) VALUES (35, 33, 'Ecrans', 0, 'ecrans');
+INSERT INTO `e-commerce`.categories (id, parent_id, name, category_order, slug) VALUES (36, 33, 'Souris', 0, 'souris');
+INSERT INTO `e-commerce`.categories (id, parent_id, name, category_order, slug) VALUES (37, null, 'Mode', 0, 'mode');
+INSERT INTO `e-commerce`.categories (id, parent_id, name, category_order, slug) VALUES (38, 37, 'Homme', 0, 'homme');
+INSERT INTO `e-commerce`.categories (id, parent_id, name, category_order, slug) VALUES (39, 37, 'Femme', 0, 'femme');
+INSERT INTO `e-commerce`.categories (id, parent_id, name, category_order, slug) VALUES (40, 37, 'Enfant', 0, 'enfant');
